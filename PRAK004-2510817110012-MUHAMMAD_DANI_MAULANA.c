c6cccca39ad5fe99cd66713ac83775359c26614d
berrypro08 <berrypro08@gmail.com>
2025-09-24 20:10:30 +0800

    Add files via upload

---
+++ PRAK004-2510817110012-MUHAMMAD_DANI_MAULANA.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#   Muhammad Dani Maulana    #\n");
+    printf("#        2510817110012       #\n");
+    printf("#                            #\n");
+    printf("##############################");
+    return 0;
+}
